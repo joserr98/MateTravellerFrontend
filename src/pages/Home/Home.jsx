887c3d0c18db0cfd24671f19335560429e03ffb7
joserr98 <joserr9807@gmail.com>
2023-06-06 19:23:28 +0200

    feat: Add home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import './Home.css'
+import Title from '../../assets/matetravellerlogo.png'
 
 export const Home = () => {
   return (
-    <div className='homeDesign'>Home</div>
+    <div className='homeDesign'>
+      <div className='homeContainer'>
+        <div className='homeTitle'><img className='homeLogo' src={Title} alt="" /></div>
+        <div className='homeSection'>
+          <p>Introducing MateTraveller, the innovative web application designed to connect solo travelers
+          with like-minded individuals exploring the world.</p> 
+          <p>Whether you're embarking on a backpacking adventure or simply craving some company during your journey, 
+          MateTraveller provides a platform where you can meet fellow travelers and forge meaningful connections along the way.</p> 
+          <p>With a user-friendly interface, you can create a MateVenture or join to a existing one to anywhere in the world. </p>
+          <p>From there, you can connect, chat, and plan adventures together, fostering new friendships and creating unforgettable memories. </p>
+          <p>Say goodbye to lonely travels and embrace the joy of shared experiences with MateTraveller.</p>
+        </div>
+      </div>
+    </div>
   )
 }
