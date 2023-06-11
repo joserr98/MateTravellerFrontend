0f8cb49c0a40bb479a1f0b2f76a6d813ee3ed354
joserr98 <joserr9807@gmail.com>
2023-06-11 20:00:28 +0200

    feat: show trips info and pagination

--- src/pages/Trips/Trips.jsx
+++ src/pages/Trips/Trips.jsx
@@ -1,16 +1,97 @@
-import React from 'react'
-import './Trips.css'
+import React, { useState, useEffect } from "react";
+import "./Trips.css";
+import { userData } from "../userSlice";
+import { useSelector } from "react-redux";
+import { getAllTrips, getPaginateTrips } from "../../services/apiCalls";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import ButtonToolbar from "react-bootstrap/ButtonToolbar";
 
 export const Trips = () => {
+  const userDataRdx = useSelector(userData);
+  const [trips, setTrips] = useState([]);
+  const [page, setPage] = useState(1);
+  const [pages, setPages] = useState("");
 
+  useEffect(() => {
+    console.log(trips);
+  });
+
+  useEffect(() => {
+    getAllTrips()
+      .then((results) => {
+        const currentPages = results.data.data.length;
+        const totalPages = currentPages / 9;
+        if (Number.isInteger(totalPages)) {
+          setPages(totalPages);
+        }
+        setPages(parseInt(totalPages) + 1);
+      })
+      .catch((err) => console.error(err));
+
+    getPaginateTrips(page)
+      .then((results) => {
+        setTrips(results.data.data.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  const setPageFunction = (newPage) => {
+    setPage(newPage);
+    getPaginateTrips(newPage)
+      .then((results) => {
+        setTrips(results.data.data.data);
+      })
+      .catch((err) => console.error(err));
+  };
 
   return (
-    <div className='tripsDeisgn'>
-
-    <div className='tripsContainer'>
-
+    <div className="tripsDeisgn">
+      {trips.length !== 0 ? (
+        <div className="tripsContainer">
+          <div className="trips">
+            {trips.map((trip) => (
+              <div className="trip">
+                <div className="tripsCity">{trip.city}</div>
+                <div className="tripsDates">
+                  <div className="startDate">{trip.start_date}</div>
+                  <div className="endDate">{trip.end_date}</div>
+                </div>
+                <div className="tripsDescription">{trip.description}</div>
+                <div><Button onClick={() => {trip.id}}>Join Trip</Button>{trip.id}</div>
+              </div>
+            ))}
+          </div>
+          <div className="pagination">
+            <ButtonToolbar aria-label="Toolbar with button groups">
+              <ButtonGroup className="me-2">
+                {page != 1 ? (
+                  <Button onClick={() => setPageFunction(1)}>1</Button>
+                ) : (
+                  <></>
+                )}
+                {page != 1 ? (
+                  <Button onClick={() => setPageFunction(page - 1)}>
+                    Previous
+                  </Button>
+                ) : (
+                  <></>
+                )}
+                <Button onClick={() => setPageFunction(page + 1)}>Next</Button>
+                {page != pages ? (
+                  <Button onClick={() => setPageFunction(pages)}>
+                    {pages}
+                  </Button>
+                ) : (
+                  <></>
+                )}
+              </ButtonGroup>
+            </ButtonToolbar>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
-
-    </div>
-  )
-}
+  );
+};
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const url = "http://localhost:8000/api"
 
+// USERS ENDPOINTS
 export const loginUser = async (credentials) => {
     return await axios.post(`${url}/login`, credentials);
 };
@@ -28,4 +29,13 @@ export const deleteUser = async (credentials) => {
       }
 
     return await axios.delete(`${url}/users/${credentials.token.id}`, config);
+};
+
+// TRIPS ENDPOINTS
+export const getAllTrips = async () => {
+    return await axios.get(`${url}/trips`);
+};
+
+export const getPaginateTrips = async (page) => {
+    return await axios.get(`${url}/trips/pages?page=${page}`);
 };
