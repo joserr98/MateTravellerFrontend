47e56f0cb1ad2c9bf785b357613a6c77750c87eb
joserr98 <joserr9807@gmail.com>
2023-06-13 10:24:45 +0200

    feat: trip card layout

--- src/pages/Trip/Trip.jsx
+++ src/pages/Trip/Trip.jsx
@@ -2,10 +2,28 @@ import React from "react";
 import "./Trip.css";
 
 export const Trip = () => {
-
   return (
     <div className="tripDeisgn">
-
+      <div className="tripContainer">
+        <div className="tripCard">
+          <div className="tripCardTitle">Málaga</div>
+          <div className="tripCardInfo">
+            <div className="tripDetails">
+              <div className="tripDate">
+                <div className="tripStartDate">StartDate</div>
+                <div className="tripEndDate">EndDate</div>
+              </div>
+              <div className="tripDescription">Description</div>
+            </div>
+            <div className="tripParticipants">
+              <div className="tripOrganizerTitle">Organizer</div>
+              <div className="tripOrganizer">Juan</div>
+              <div className="tripParticipantsTitle">Participants</div>
+              <div className="tripTraveler">Jose</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
