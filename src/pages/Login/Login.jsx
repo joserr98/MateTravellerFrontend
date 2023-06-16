ebf6422a933ee7a27c21906961b4d2ce983df9dd
joserr98 <joserr9807@gmail.com>
2023-06-16 11:03:25 +0200

    fix: small fix

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
-import "./login.css";
+import "./Login.css";
 import Title from "../../assets/finallogo.png";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
