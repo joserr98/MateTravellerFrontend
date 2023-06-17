99f9b813854ef2ce07099281e9801026f2d0eb01
joserr98 <joserr9807@gmail.com>
2023-06-17 18:46:33 +0200

    feat: add redirection if page does not exist

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -14,6 +14,7 @@ export const Body = () => {
 
   return (
     <Routes>
+        <Route path="/*" element={<Home/>} />
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register/>} />
